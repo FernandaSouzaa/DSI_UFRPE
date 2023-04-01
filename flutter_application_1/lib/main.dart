5e321fc1565a7fa883844fb4715cad7a8707bf9a
FernandaSouzaa <104859098+FernandaSouzaa@users.noreply.github.com>
2023-04-01 11:33:12 -0300

    favorite and cardmode

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,58 +1,219 @@
-import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Startup Name Generator'),
-        ),
-        body: const Center(
-          child: RandomWords(),
-        ),
+        title: 'Statup name generator',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 62, 178, 99),
+          foregroundColor: Color.fromARGB(255, 255, 255, 255),
+        )),
+        home: const RandomWords());
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
+  final _biggerFont = const TextStyle(fontSize: 20);
+  bool switchMode = false;
+
+  void _pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute<void>(
+      builder: (context) {
+        final tiles = _saved.map((pair) {
+          return ListTile(
+            title: Text(
+              pair.asPascalCase,
+              style: _biggerFont,
+            ),
+          );
+        });
+        final divided = tiles.isNotEmpty
+            ? ListTile.divideTiles(context: context, tiles: tiles).toList()
+            : <Widget>[];
+
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("Saved suggestions"),
+          ),
+          body: ListView(children: divided),
+        );
+      },
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+        actions: [
+          IconButton(
+            onPressed: _pushSaved,
+            icon: const Icon(Icons.save_as_outlined),
+            tooltip: "Saved suggestions",
+          )
+        ],
+      ),
+      body: switchMode
+          ? CardMode(
+              suggestions: _suggestions, saved: _saved, biggerFont: _biggerFont)
+          : DefaultMode(
+              suggestions: _suggestions,
+              saved: _saved,
+              biggerFont: _biggerFont),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            switchMode = !switchMode;
+          });
+        },
+        backgroundColor: const Color.fromARGB(255, 62, 178, 99),
+        child: const Icon(Icons.change_circle_outlined),
       ),
     );
   }
 }
 
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18);
+class DefaultMode extends StatefulWidget {
+  final List<WordPair> suggestions;
+  final Set<WordPair> saved;
+  final TextStyle biggerFont;
+  const DefaultMode(
+      {super.key,
+      required this.suggestions,
+      required this.saved,
+      required this.biggerFont});
 
+  @override
+  State<DefaultMode> createState() => _DefaultModeState();
+}
+
+class _DefaultModeState extends State<DefaultMode> {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       padding: const EdgeInsets.all(16.0),
-      itemBuilder: /*1*/ (context, i) {
-        if (i.isOdd) return const Divider(); /*2*/
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
 
-        final index = i ~/ 2; /*3*/
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+        final index = i ~/ 2;
+        if (index >= widget.suggestions.length) {
+          widget.suggestions.addAll(generateWordPairs().take(10));
         }
+        final alreadySaved = widget.saved.contains(widget.suggestions[index]);
         return ListTile(
           title: Text(
-            _suggestions[index].asPascalCase,
-            style: _biggerFont,
+            widget.suggestions[index].asPascalCase,
+            style: widget.biggerFont,
           ),
+          trailing: Icon(
+            alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? const Color.fromARGB(255, 202, 35, 29) : null,
+            semanticLabel: alreadySaved ? "Remove from saved" : "Save",
+          ),
+          onTap: () {
+            setState(() {
+              if (alreadySaved) {
+                widget.saved.remove(widget.suggestions[index]);
+              } else {
+                widget.saved.add(widget.suggestions[index]);
+              }
+            });
+          },
         );
       },
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({super.key});
+class CardMode extends StatefulWidget {
+  final List<WordPair> suggestions;
+  final Set<WordPair> saved;
+  final TextStyle biggerFont;
+  const CardMode(
+      {super.key,
+      required this.suggestions,
+      required this.saved,
+      required this.biggerFont});
 
   @override
-  State<RandomWords> createState() => _RandomWordsState();
+  State<CardMode> createState() => _CardModeState();
+}
+
+class _CardModeState extends State<CardMode> {
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        padding: const EdgeInsets.all(15),
+        itemBuilder: ((context, i) {
+          if (i >= widget.suggestions.length - 1) {
+            widget.suggestions.addAll(generateWordPairs().take(10));
+          }
+          final alreadySaved = widget.saved.contains(widget.suggestions[i]);
+          return Row(
+            children: [
+              Flexible(
+                child: Container(
+                  padding: const EdgeInsets.fromLTRB(7, 7, 0, 0),
+                  height: 160,
+                  width: double.maxFinite,
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        if (alreadySaved) {
+                          widget.saved.remove(widget.suggestions[i]);
+                        } else {
+                          widget.saved.add(widget.suggestions[i]);
+                        }
+                      });
+                    },
+                    child: Card(
+                      elevation: 2,
+                      child: Center(
+                          child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text("${widget.suggestions[i]} Card",
+                              style: widget.biggerFont),
+                          Icon(
+                            alreadySaved
+                                ? Icons.favorite
+                                : Icons.favorite_border,
+                            color: alreadySaved
+                                ? const Color.fromARGB(255, 202, 35, 29)
+                                : null,
+                            semanticLabel:
+                                alreadySaved ? "Remove from saved" : "Save",
+                          )
+                        ],
+                      )),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          );
+        }));
+  }
 }
